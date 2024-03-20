7b694b5f14be1abf6a3ac32785e9bd7a1e665c21
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-03-20 20:41:14 +0200

    done style for sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,18 +5,11 @@ export default function Sidebar() {
     return (
         <div className='sidebar'>
             <div className="top">   
-                logo
-                <img className='menu' src={assets.bulb_icon} alt="" />
-            </div>
+                <img className='menu' src={assets.menu_icon} alt="" />
             <div className='new-chat'>
                     <img src={assets.plus_icon} alt="" />
                     <p>New Chat</p>
                 </div>
-            
-
-            <div className="top">   
-                <img className='menu' src={assets.menu_icon} alt="" />
-               
                 <div className='recent'>
                     <p className="recent-title">Recent</p>
                     <div className="recent-entry">
@@ -25,6 +18,7 @@ export default function Sidebar() {
                     </div>
                 </div>
             </div>
+
 
 
             <div className="bottom">
