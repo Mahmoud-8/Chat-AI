3ed584e478ec4b69dcf770aa0c1930c37480a327
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-03-21 20:51:09 +0200

    done Test api from google

---
+++ src/context/Context.jsx
@@ -0,0 +1,22 @@
+import { createContext } from "react";
+import runChat from "../config/Hepha";
+
+export const Context = createContext();
+const ContextProvider = (props) => {
+    const onSent = async (prompt) => {
+        runChat(prompt)
+    }
+
+    // onSent("where is Egypt?")
+
+    const contextValue = {
+
+    }
+
+    return (
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+    )
+}
+export default ContextProvider
