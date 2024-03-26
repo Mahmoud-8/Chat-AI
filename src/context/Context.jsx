7c40d5a6d7c68af00503b49878e3dcfad7f29c1c
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-03-26 21:38:16 +0200

    fix the auto scroll

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,11 +1,11 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useRef, useEffect } from "react";
 import runChat from "../config/Hepha";
+
 export const ChatContext = createContext();
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
-
     const [input, setInput] = useState("");
     const [recentPrompt, setRecentPrompt] = useState("");
     const [prevPrompts, setPrevPrompts] = useState([]);
@@ -13,64 +13,62 @@ const ContextProvider = (props) => {
     const [loading, setLoading] = useState(false);
     const [resultData, setResultData] = useState("");
 
+    const resultRef = useRef(null); // Create a ref for the result container
+
     const delayPara = (index, nextWord) => {
         setTimeout(function () {
-            setResultData(prev => prev + nextWord); 
-        },75*index)
-    }
+            setResultData((prev) => prev + nextWord);
+        }, 75 * index);
+    };
+
+    useEffect(() => {
+        if (resultRef.current) {
+            resultRef.current.scrollIntoView({ behavior: "smooth" }); // Scroll to the bottom
+        }
+    }, [resultData]); // Scroll whenever resultData changes
 
     const newChat = () => {
-        setLoading(false)
-        setShowResult(false)
-    }
-
+        setLoading(false);
+        setShowResult(false);
+    };
 
     const onSent = async (prompt) => {
-
-        setResultData("")
-        setLoading(true)
-        setShowResult(true)
+        setResultData("");
+        setLoading(true);
+        setShowResult(true);
         let response;
         if (prompt !== undefined) {
             response = await runChat(prompt);
-            setRecentPrompt(prompt)
-
-        }
-        else
-        {
-            setPrevPrompts(prev => [...prev, input])
-            setRecentPrompt(input)
+            setRecentPrompt(prompt);
+        } else {
+            setPrevPrompts((prev) => [...prev, input]);
+            setRecentPrompt(input);
             response = await runChat(input);
         }
-       
-       let responseArray = response.split("**");
 
-       let newResponse= "";
-       for(let i = 0; i < responseArray.length; i++)
-       {
-           if (i === 0 || i % 2 !== 1) {
-            newResponse += responseArray[i]; 
+        let responseArray = response.split("**");
 
-           } 
-           else {
-            newResponse += "<b>" + responseArray[i]+ "</b>";
-           }
-           }
-           let newResponse2 = newResponse.split("*").join("</br>")
-           
-           let newResponseArray = newResponse2.split(" ");
-           for (let i = 0; i < newResponseArray.length; i++)
-               {
-                const nextWord = newResponseArray[i];
-                delayPara(i,nextWord+ " ");
-              }
+        let newResponse = "";
+        for (let i = 0; i < responseArray.length; i++) {
+            if (i === 0 || i % 2 !== 1) {
+                newResponse += responseArray[i];
+            } else {
+                newResponse += "<b>" + responseArray[i] + "</b>";
+            }
+        }
+        let newResponse2 = newResponse.split("*").join("</br>");
+
+        let newResponseArray = newResponse2.split(" ");
+        for (let i = 0; i < newResponseArray.length; i++) {
+            const nextWord = newResponseArray[i];
+            delayPara(i, nextWord + " ");
+        }
 
 
-         setLoading(false)
-         setInput("")
-    }
 
-    //   onSent("where is sweden?")
+        setLoading(false);
+        setInput("");
+    };
 
     const contextValue = {
         prevPrompts,
@@ -87,12 +85,12 @@ const ContextProvider = (props) => {
         showResult,
         setLoading,
         setResultData,
-    }
+    };
 
     return (
         <Context.Provider value={contextValue}>
             {props.children}
         </Context.Provider>
-    )
-}
-export default ContextProvider
+    );
+};
+export default ContextProvider;
